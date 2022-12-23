e4d3813d8627802220881b655b9cad0221bfb51d
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2022-12-23 11:44:26 +0100

    Creating anonymous strings

---
+++ DemoCode.Tests/NumberDemos.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using FluentAssertions;
+
+namespace DemoCode.Tests
+{
+    public class NumberDemos
+    {
+        [Fact]
+        public void Ints()
+        {
+            // arrange
+            var sut = new IntCalculator();
+            var fixture = new Fixture();
+
+            // act
+            sut.Subtract(fixture.Create<int>());
+
+            // assert
+            sut.Value.Should().BeLessThan(0);
+        }
+    }
+}
---
+++ DemoCode.Tests/StringDemos.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class StringDemos
+    {
+        [Fact]
+        public void BasicStrings()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            var firstName = fixture.Create("First_");
+            var lastName = fixture.Create("Last_");
+
+            // act
+            var result = sut.Join(firstName, lastName);
+
+            // assert
+            result.Should().BeEquivalentTo(firstName + " " + lastName);        }
+    }
+}
